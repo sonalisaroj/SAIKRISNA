9e1498b31148db4bfbafc2af8e3d408659bb466d
saikrisna <chhatoisaroj07@gmail.com>
2021-03-30 21:10:52 +0530

    New Project

---
+++ src/test/java/com/saikrisna/GitLearning/GitLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saikrisna.GitLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
